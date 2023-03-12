b4a5a010d77def104a0ef12c1d6900058073dc7e
srctwd <srctwd@aisha.aisha>
2023-03-12 15:30:59 -0300

    Volumetry for each month on batch_records.

--- monitoring/app/api/endpoints/performance.py
+++ monitoring/app/api/endpoints/performance.py
@@ -1,9 +1,13 @@
 #Endpoint for performance calculations.
 from fastapi import APIRouter, Request
+import pandas as pd
 
 router = APIRouter(prefix="/performance")
 
 @router.post("")
 async def performance(records: Request):
     data = await records.json()
-    return 21
+    df = pd.DataFrame(data)
+    response = df["REF_DATE"].str[:7].value_counts().to_dict()
+    
+    return response
